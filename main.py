31f389aa844b6069e5068609f1cefefa1cca790f
Columbus Feller <sammy.feller@gmail.com>
2021-09-18 16:00:31 -0400

    Question 4 - Fixed

--- main.py
+++ main.py
@@ -1,42 +1,32 @@
-# Importing the required libraries
+
 from matplotlib import pyplot as plt
 import numpy as np
-#import os package to use file related methods
 import os
-
 from sklearn import *
+from sklearn.feature_extraction.text import CountVectorizer
 
 directories = ["Datasets\\BBC\\business", "Datasets\\BBC\\entertainment", "Datasets\\BBC\\politics", "Datasets\\BBC\\sport" ,"Datasets\\BBC\\tech"]
 categories = ["Business", "Entertainment", "Politics", "Sport", "Technology"]
 
-#initialization of file directories
 def plot_bbc_groups():
     nbrFilesPerCtgry = []
-
     for directory in directories:
         dirListing = os.listdir(directory)
         nbrFilesPerCtgry.append(len(dirListing))
-
-    # Creating a bar chart with the parameters
     plt.bar(categories, nbrFilesPerCtgry, width=0.7, bottom=50)
     plt.xlabel('Categories', fontsize=15)
     plt.ylabel('Files', fontsize=15)
     for i in range(len(categories)):
         plt.text(i, nbrFilesPerCtgry[i], nbrFilesPerCtgry[i], ha='center', va='bottom')
-    #plt.show()
+    plt.show()
     plt.savefig("Results//BBC-distribution.pdf", dpi = 100)
 
 def assign_category_name():
-
-    data = datasets.load_files("Datasets\\BBC", encoding = "latin1")
-
-    vectorized = feature_extraction.text.CountVectorizer(input='data')
-    matrix = vectorized.fit_transform(data)
-    print(vectorized.vocabulary)
+    text_train_subset = datasets.load_files("Datasets\\red", encoding = "latin1")
+    vectorizer = CountVectorizer()
+    X_train = vectorizer.fit_transform(text_train_subset.data)
+    print(list(zip(vectorizer.get_feature_names(), X_train.sum(0).getA1())))
     
-    
-
-
 #plot_bbc_groups()
 assign_category_name()
 
