b5564f8d5f08060984b239d7e49170570f6fec9e
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2021-10-07 20:40:53 -0400

    latest version
    has smoothing parameter

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ from sklearn.metrics import classification_report, accuracy_score, confusion_mat
 directories = ["Datasets\\BBC\\business", "Datasets\\BBC\\entertainment", "Datasets\\BBC\\politics", "Datasets\\BBC\\sport" ,"Datasets\\BBC\\tech"]
 categories = ["Business", "Entertainment", "Politics", "Sport", "Technology"]
 
+bbc_loaded_files = load_files("Datasets\\BBC", encoding = "latin1")
+X_train, X_test, Y_train, Y_test = train_test_split(bbc_loaded_files.data, bbc_loaded_files.target, test_size=0.2, random_state = None)
+vectorizer = CountVectorizer()
+vectorizer.fit(X_train)
+doc_matrix = vectorizer.transform(X_train)
+#print(vectorizer.get_feature_names())
+labels, counts = np.unique(bbc_loaded_files.target, return_counts=True)
+labels_str = np.array(bbc_loaded_files.target_names)[labels]
+
 def plot_bbc_groups():
     '''
     TASK 1 - Question 2
@@ -29,7 +38,7 @@ def plot_bbc_groups():
     plt.show()
     plt.savefig("Results//BBC-distribution.pdf", dpi = 100)
 
-def assign_category_name():
+def assign_category_name(bbc_loaded_files,smoothing):
     '''
     TASK 1 - Question 3-4-5
     3 - Loads the corpus
@@ -37,48 +46,52 @@ def assign_category_name():
     5 - Splits the dataset into 80% for training and 20% for testing
     '''
     'Question 3'
-    bbc_loaded_files = load_files("Datasets\\BBC", encoding = "latin1")
+    
     #verify the count of each category and get the target names (categories) and store as 'labels_str' now labels is value 0-4
-    labels, counts = np.unique(bbc_loaded_files.target, return_counts=True)
-    labels_str = np.array(bbc_loaded_files.target_names)[labels]
-    print(labels_str,counts)
+    
+    #print(labels_str,counts)
+
     'Question 5'
     #bbc_loadfiles.data is the data itself
     #bbc_loadfiles.target is the labels/categories
-    X_train, X_test, Y_train, Y_test = train_test_split(bbc_loaded_files.data, bbc_loaded_files.target, test_size=0.2, random_state = None)
+    
     #print('80%:',len(X_train),'20%:',len(X_test))
+
     'Question 4'
     #it is best to use only the testing set to form our matrix
-    vectorizer = CountVectorizer()
-    vectorizer.fit(X_train)
-    doc_matrix = vectorizer.transform(X_train)
-    #print(vectorizer.get_feature_names())
+
     'Question 6/7a'
-    clf = MultinomialNB()
+    #clf stands for classifier
+    clf = MultinomialNB(alpha=smoothing)
     #transform the data to document term matrix and pass labels
     clf.fit(doc_matrix, Y_train)
     Y_pred = clf.predict(vectorizer.transform(X_test))
     #print(clf.feature_count_)
-    #TODO turn above in to variable and iterate to count word count per row.
     'Question 7'
-    #print(confusion_matrix(Y_test, Y_pred)) #b
-    #print(classification_report(Y_test, Y_pred)) #c
-    #print(accuracy_score(Y_test, Y_pred)) #d
-    print('--------------------------------------------------------------------------------------------')
+    print('Confusion Matrix: ')
+    print(confusion_matrix(Y_test, Y_pred)) #b
+    print('\nClassification Report: ')
+    print(classification_report(Y_test, Y_pred)) #c
+    print('\nAccuracy Score: ')
+    print(accuracy_score(Y_test, Y_pred)) #d
+    print('\nF1 score, macro: ')
     print(f1_score(Y_test, Y_pred, average='macro'))#d pt2
+    print('\nF1 score, weighted: ')
     print(f1_score(Y_test, Y_pred, average='weighted')) #d pt3
+    print('\nPrio probability of each class: ')
     print(labels_str, counts/len(bbc_loaded_files.target)) #e
+    print('\nTotal Number of words in the vocabulary: ')
     print(len(vectorizer.vocabulary_)) #f the total number of words in the vocabulary
-    print('-------------------------------------------G/H/I/J-------------------------------------------')
     #7g
-    print(clf.classes_)
+    #print(clf.classes_)
     word_count_per_label = 0
-    total_words_in_corpus = 0
-    total_one_count_words_in_corpus = 0
+    total_words_in_X_train = 0
+    total_one_count_words_in_X_train = 0
     matrix_array = clf.feature_count_
     zero_count_for_this_label = 0
     one_count_for_this_label = 0
     curr_lbl = 0
+    print("Gathering numerical data per label...")
     for label in matrix_array:
         #label prints out the array of a label/category
         print(curr_lbl,': ',label)
@@ -90,21 +103,29 @@ def assign_category_name():
                 word_count_per_label = word_count_per_label + count_for_this_word
             else:
                 word_count_per_label = word_count_per_label + count_for_this_word
-        print('Word count for this label: ',word_count_per_label) #g
-        print('Number of zero counts per word found in this label: ', zero_count_for_this_label, ' Percentage: ', round(((zero_count_for_this_label/word_count_per_label)*100),2),'%')#I
-        total_words_in_corpus = word_count_per_label + total_words_in_corpus
-        total_one_count_words_in_corpus = one_count_for_this_label + total_one_count_words_in_corpus
+        print(curr_lbl,':G, Word count for this label: ',word_count_per_label) #g
+        print(curr_lbl,':I, Number of zero counts per word found in this label: ', zero_count_for_this_label, ' Percentage: ', round(((zero_count_for_this_label/word_count_per_label)*100),2),'%')#I
+        print('-------------------')
+        total_words_in_X_train = word_count_per_label + total_words_in_X_train
+        total_one_count_words_in_X_train = one_count_for_this_label + total_one_count_words_in_X_train
         zero_count_for_this_label = 0
         word_count_per_label = 0
         curr_lbl = curr_lbl + 1
-    print('\ntotal words in all folders/files: ', total_words_in_corpus)#h
-    print('Number of singular words in corpus: ', total_one_count_words_in_corpus, 'Percentage: ',round(((total_one_count_words_in_corpus/total_words_in_corpus)*100),2),'%')#J
-    print('--------------------------------------------------------------------------------------------')
-    docmatrix_toarray = doc_matrix.toarray()
-    test = pd.DataFrame(docmatrix_toarray, columns=vectorizer.get_feature_names())
-    print(test)
-
+    print('\nH, total words in all folders/files: ', total_words_in_X_train)#h
+    print('J, Number of singular words in corpus: ', total_one_count_words_in_X_train, 'Percentage: ',round(((total_one_count_words_in_X_train/total_words_in_X_train)*100),2),'%')#J
+    print("-"*58)
+    #docmatrix_toarray = doc_matrix.toarray()
+    #test = pd.DataFrame(docmatrix_toarray, columns=vectorizer.get_feature_names())
+    #print((clf.feature_log_prob_)[0,0])
 
 #plot_bbc_groups()
-assign_category_name()
+
+print("-"*20,"try 1","-"*20)
+assign_category_name(bbc_loaded_files, 1.0)
+print("-"*20,"try 2","-"*20)
+assign_category_name(bbc_loaded_files, 1.0)
+print("-"*20,"Smoothing 0.0001","-"*20)
+assign_category_name(bbc_loaded_files, 0.0001)
+print("-"*20,"Smoothing 0.9","-"*20)
+assign_category_name(bbc_loaded_files, 0.9)
 
